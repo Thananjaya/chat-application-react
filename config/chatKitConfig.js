32a0cb1b48fd12e677017bed5879e0f87daa8218
Thananjaya <thananjaya091993@gmail.com>
2019-04-26 15:54:43 +0530

    added css anf pusher config

---
+++ config/chatKitConfig.js
@@ -0,0 +1,4 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/bc451ba6-d16a-48e5-a0ca-cc5004a730c7/token";
+const instance = "v1:us1:bc451ba6-d16a-48e5-a0ca-cc5004a730c7";
+
+export { tokenUrl, instance }
